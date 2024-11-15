154e4db4fcc55fc0553059396b8f36df36bd9fb8
Jobbet789 <77113631+Jobbet789@users.noreply.github.com>
2024-11-15 12:10:39 +0100

    Pygame Ball object

---
+++ lib/Ball.py
@@ -0,0 +1,17 @@
+import math
+import pygame
+
+class Ball:
+    def __init__(self, x: int, y: int):
+        self.x = x
+        self.y = y
+        self.direction = 0
+        self.speed = 0
+
+    def move(self):
+        self.x += self.speed * math.cos(self.direction)
+        self.y += self.speed * math.sin(self.direction)
+
+    def draw(self, screen):
+        pygame.draw.circle(screen, (255, 0, 0), (int(self.x), int(self.y)), 10)
+        pygame.draw.line(screen, (0, 0, 255), (self.x, self.y), (self.x + 20 * math.cos(self.direction), self.y + 20 * math.sin(self.direction)), 5)
