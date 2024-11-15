f4bc2c79f51362d6f8e881b25bec177fb4c53329
Jobbet789 <77113631+Jobbet789@users.noreply.github.com>
2024-11-15 12:06:52 +0100

    Pygame robot object

---
+++ lib/Robot.py
@@ -0,0 +1,28 @@
+import math
+import pygame
+
+# Two motors, alternate their speed to change direction
+class Robot:
+    def __init__(self):
+        self.x, self.y = 0, 0 # coords
+        self.speed1, self.speed2 = 0, 0 # motor speeds
+        self.heading = 0
+
+    def calculate_angular_speed(self):
+        return (self.speed1 - self.speed2) / 2
+
+    def calculate_speed(self):
+        return (self.speed1 + self.speed2) / 2 
+   
+    def move(self):
+        self.x += self.calculate_speed() * math.cos(self.heading)
+        self.y += self.calculate_speed() * math.sin(self.heading)
+        self.heading += self.calculate_angular_speed()
+
+    def set_speed(self, speed1, speed2):
+        self.speed1 = speed1
+        self.speed2 = speed2
+    
+    def draw(self, screen):
+        pygame.draw.circle(screen, (255, 0, 0), (int(self.x), int(self.y)), 10)
+        pygame.draw.line(screen, (0, 0, 255), (self.x, self.y), (self.x + 20 * math.cos(self.heading), self.y + 20 * math.sin(self.heading)), 5)
